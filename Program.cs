637cf7f3a4a6c5fc3fc09685132987d75e78653a
AnnaNorlyk <148754654+AnnaNorlyk@users.noreply.github.com>
2025-02-24 21:12:23 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DevOps_Secrets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
